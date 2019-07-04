7ac94810ec75fc4f6885cb823fdf15faebfb2d4c
Paul Stancliffe <paul.stancliffe@gmail.com>
2019-07-04 19:32:31 +0100

    feat: number of lives added

--- game_functions.py
+++ game_functions.py
@@ -124,19 +124,23 @@ def change_fleet_direction(ai_settings, aliens):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """ Respond to ship being hit by alien. """
-    # Decrement ships left.
-    stats.ships_left -= 1
+    if stats.ships_left > 0:
+        # Decrement ships left.
+        stats.ships_left -= 1
 
-    # Empty the list of aliens and bullets
-    aliens.empty()
-    bullets.empty()
+        # Empty the list of aliens and bullets
+        aliens.empty()
+        bullets.empty()
 
-    # Create a new fleet and center the ship.
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
+        # Create a new fleet and center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
-    # Pause
-    sleep(0.5)
+        # Pause
+        sleep(0.5)
+
+    else:
+        stats.game_active = False
 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """ Check if the fleet is at the edge,
@@ -148,6 +152,17 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 
+    # Look for aliens hitting the bottom of the screen
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """ Check if any aliens have reached the bottom of the screen. """
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Treat this the same as if the ship got hit.
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
 
 
 
--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 5
         self.ship_limit = 3
 
         # Bullet settings
@@ -20,7 +20,7 @@ class Settings():
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed_factor = 5
+        self.alien_speed_factor = 3
         self.fleet_drop_speed = 10
         # fleet direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
