9aa37dadade8919769b98873b274fa95e1ce7f73
Paul Stancliffe <paul.stancliffe@gmail.com>
2019-07-04 10:51:11 +0100

    feat: adding first alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -14,6 +15,8 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make an alien.
+    alien = Alien(ai_settings, screen)
     # Make a group to store bullets in.
     bullets = Group()
 
@@ -22,7 +25,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
